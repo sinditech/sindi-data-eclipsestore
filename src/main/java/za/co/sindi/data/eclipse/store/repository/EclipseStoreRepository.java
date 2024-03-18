44c93303b01019925b4dcae0df1da5a9b054a675
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-03-18 15:49:57 +0200

    Initial Commit.

---
+++ src/main/java/za/co/sindi/data/eclipse/store/repository/EclipseStoreRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package za.co.sindi.data.eclipse.store.repository;
+
+import za.co.sindi.data.repository.CrudRepository;
+
+/**
+ * @author Buhake Sindi
+ * @since 19 November 2023
+ */
+public interface EclipseStoreRepository<E, ID> extends CrudRepository<E, ID> {
+
+}
